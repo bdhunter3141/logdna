bddfe3dfdbf7b5ed98cb8df349cfd5eba5490395
Bryan Hunter <bdhunter3141@gmail.com>
2019-12-18 16:48:44 -0500

    sidebar styling.

--- logdna-frontend/src/Components/ConversationListSidebar.js
+++ logdna-frontend/src/Components/ConversationListSidebar.js
@@ -3,8 +3,11 @@ import '../Styles/ConversationListSidebar.css';
 
 function ConversationListSidebar() {
   return (
-    <div>
-      TEMP
+    <div className="Conversation-list-container">
+      <div className="conversation">
+        <p className="conversation-title">TEMP</p>
+        <p>TEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP TempTEMP TEMP Temp</p>
+      </div>
     </div>
   );
 }
