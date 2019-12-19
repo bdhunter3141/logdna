fa12833acc373ad03e4529b5f93efe9c1d4badd4
Bryan Hunter <bdhunter3141@gmail.com>
2019-12-18 23:29:10 -0500

    fixed content overflow issue in sidebar and initial styling of message form

--- logdna-frontend/src/Components/ConversationListSidebar.js
+++ logdna-frontend/src/Components/ConversationListSidebar.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import '../Styles/ConversationListSidebar.css';
 
-let conversations;
+class ConversationListSidebar extends React.Component {
 
-// Generate the test conversations
-(function () {
-  let convoArr = []
-  let content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean accumsan sollicitudin felis, non elementum erat rutrum id. Curabitur eu metus nec dui lacinia placerat condimentum sed elit. In luctus vel velit quis molestie. Integer vitae blandit neque, et tincidunt velit. Quisque posuere vitae urna iaculis lacinia. Duis egestas varius pretium. Sed mollis venenatis lorem, vel faucibus urna. Sed non interdum urna, eget consequat risus. Nunc at lectus leo. Suspendisse potenti. Nullam eu accumsan arcu. Duis molestie lorem neque. Pellentesque quis risus suscipit, congue mauris nec, accumsan libero. In hac habitasse platea dictumst. Maecenas vitae fermentum augue.'
+  state = {
+    conversations: []
+  }
+
+  componentDidMount() {
+    this.handleContentLength()
+    window.addEventListener('resize', this.handleContentLength)
+  }
+
   // Trim down the content to a preview length and add ellipses
-  if (content.length > 175) {
-    content = content.substr(0, 175)
-    content = `${content}...`
+  handleContentLength = () => {
+    let convoArr = []
+    let content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean accumsan sollicitudin felis, non elementum erat rutrum id. Curabitur eu metus nec dui lacinia placerat condimentum sed elit. In luctus vel velit quis molestie. Integer vitae blandit neque, et tincidunt velit. Quisque posuere vitae urna iaculis lacinia. Duis egestas varius pretium. Sed mollis venenatis lorem, vel faucibus urna. Sed non interdum urna, eget consequat risus. Nunc at lectus leo. Suspendisse potenti. Nullam eu accumsan arcu. Duis molestie lorem neque. Pellentesque quis risus suscipit, congue mauris nec, accumsan libero. In hac habitasse platea dictumst. Maecenas vitae fermentum augue.'
+    let containerWidth = document.getElementsByClassName('Conversation-list-container')[0].offsetWidth
+    let contentTrimLength = containerWidth >= 170 ? (containerWidth * 0.4) : (containerWidth * 0.3) || 0;
+    if (content.length > contentTrimLength) {
+      content = content.substr(0, contentTrimLength)
+      content = `${content}...`
+    }
+    for (let i = 0; i <= 14; i++) {
+      convoArr.push({title: 'Lorem Ipsum', content})
+    }
+    this.setState({conversations: convoArr})
   }
-  for (let i = 0; i <= 14; i++) {
-    convoArr.push({title: 'Lorem Ipsum', content})
-  }
-  conversations = convoArr
-})()
 
-
-function ConversationListSidebar() {
-
-  // Handle selecting conversation
-  function handleClick(e) {
+  // Style selected conversations
+  handleClick = (e) => {
     e.preventDefault()
     let currentlySelected = document.getElementsByClassName('selected')
     if (currentlySelected.length) {
@@ -33,23 +40,25 @@ function ConversationListSidebar() {
     e.currentTarget.classList.add('selected')
   }
 
-  return (
-    <div className="Conversation-list-container">
+  render() {
+    let {conversations} = this.state
 
-      { conversations.map( (conversation, i) => {
-        return (
-          <div className={`conversation ${i === 0 ? 'selected' : ''}`} key={i} onClick={handleClick}>
-            <div>
-              <p className="conversation-title">{conversation.title}</p>
-              <p className="conversation-content">{conversation.content}</p>
+    return (
+      <div className="Conversation-list-container">
+        { conversations.map( (conversation, i) => {
+          return (
+            <div className={`conversation ${i === 0 ? 'selected' : ''}`} key={i} onClick={this.handleClick}>
+              <div>
+                <p className="conversation-title">{conversation.title}</p>
+                <p className="conversation-content">{conversation.content}</p>
+              </div>
+              <div className="triangle"></div>
             </div>
-            <div className="triangle"></div>
-          </div>
-        )
-      }) }
-
-    </div>
-  );
+          )
+        }) }
+      </div>
+    );
+  }
 }
 
 export default ConversationListSidebar;
---
+++ logdna-frontend/src/Components/MessageForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/MessageForm.css';
+
+function MessageForm() {
+  return (
+    <div className="Message-form-container">
+      <div>Message</div>
+      <div>Submit</div>
+    </div>
+  );
+}
+
+export default MessageForm;
