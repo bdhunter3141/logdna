bb5b9ad44d88948d01df2978c3f487e98435f6f3
Bryan Hunter <bdhunter3141@gmail.com>
2019-12-19 16:44:44 -0500

    renamed frontend to client and setup initial individual message structure

---
+++ client/src/Components/IndividualMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/IndividualMessage.css';
+
+function IndividualMessage() {
+  return (
+    <div className="Individual-message-container">
+      PLACEHOLDER
+    </div>
+  );
+}
+
+export default IndividualMessage;
