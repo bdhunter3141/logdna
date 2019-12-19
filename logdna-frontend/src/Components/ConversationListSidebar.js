8ed7d1c18655230a325ae1147a51c97b0b595ec2
Bryan Hunter <bdhunter3141@gmail.com>
2019-12-19 11:42:46 -0500

    completed message form styling

--- logdna-frontend/src/Components/ConversationListSidebar.js
+++ logdna-frontend/src/Components/ConversationListSidebar.js
@@ -4,6 +4,8 @@ import '../Styles/ConversationListSidebar.css';
 class ConversationListSidebar extends React.Component {
 
   state = {
+    title: 'Lorem Ipsum',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean accumsan sollicitudin felis, non elementum erat rutrum id. Curabitur eu metus nec dui lacinia placerat condimentum sed elit. In luctus vel velit quis molestie.Integer vitae blandit neque, et tincidunt velit. Quisque posuere vitae urna iaculis lacinia. Duis egestas varius pretium. Sed mollis venenatis lorem, vel faucibus urna. Sed non interdum urna, eget consequat risus. Nunc at lectus leo. Suspendisse potenti. Nullam eu accumsan arcu. Duis molestie lorem neque. Pellentesque quis risus suscipit, congue mauris nec, accumsan libero. In hac habitasse platea dictumst. Maecenas vitae fermentum augue.',
     conversations: []
   }
 
@@ -11,21 +13,35 @@ class ConversationListSidebar extends React.Component {
     this.handleContentLength()
     window.addEventListener('resize', this.handleContentLength)
   }
-
+  
   // Trim down the content to a preview length and add ellipses
   handleContentLength = () => {
     let convoArr = []
-    let content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean accumsan sollicitudin felis, non elementum erat rutrum id. Curabitur eu metus nec dui lacinia placerat condimentum sed elit. In luctus vel velit quis molestie. Integer vitae blandit neque, et tincidunt velit. Quisque posuere vitae urna iaculis lacinia. Duis egestas varius pretium. Sed mollis venenatis lorem, vel faucibus urna. Sed non interdum urna, eget consequat risus. Nunc at lectus leo. Suspendisse potenti. Nullam eu accumsan arcu. Duis molestie lorem neque. Pellentesque quis risus suscipit, congue mauris nec, accumsan libero. In hac habitasse platea dictumst. Maecenas vitae fermentum augue.'
+    let {content, title} = this.state
     let containerWidth = document.getElementsByClassName('Conversation-list-container')[0].offsetWidth
-    let contentTrimLength = containerWidth >= 170 ? (containerWidth * 0.4) : (containerWidth * 0.3) || 0;
+    let containerWidthMultiplier = this.getContainerWidthMultiplier(containerWidth)
+    let contentTrimLength = containerWidth * containerWidthMultiplier;
     if (content.length > contentTrimLength) {
       content = content.substr(0, contentTrimLength)
       content = `${content}...`
     }
     for (let i = 0; i <= 14; i++) {
-      convoArr.push({title: 'Lorem Ipsum', content})
+      convoArr.push({title, content})
     }
     this.setState({conversations: convoArr})
+  }
+
+  // Return a number to multiply the container width by to get an acceptable content length
+  getContainerWidthMultiplier = (containerWidth) => {
+    let containerWidthMultiplier
+    if (containerWidth >= 330) {
+      containerWidthMultiplier = 0.5
+    } else if (containerWidth < 330 && containerWidth >= 170) {
+      containerWidthMultiplier = 0.397
+    } else {
+      containerWidthMultiplier = 0.3
+    }
+    return containerWidthMultiplier
   }
 
   // Style selected conversations
--- logdna-frontend/src/Components/MessageForm.js
+++ logdna-frontend/src/Components/MessageForm.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import '../Styles/MessageForm.css';
 
-function MessageForm() {
+class MessageForm extends React.Component {
+  state = {
+    message: ''
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  handleInput = (e) => {
+    this.setState({message: e.target.value})
+  }
+
+  render() {
   return (
-    <div className="Message-form-container">
-      <div>Message</div>
-      <div>Submit</div>
-    </div>
-  );
+    <form className="Message-form-container" onSubmit={this.handleSubmit}>
+      <input type='text' name='message' placeholder='Message...' onChange={this.handleInput} />
+      <button type='submit' value="Submit">Submit</button>
+    </form>
+  )};
 }
 
 export default MessageForm;
