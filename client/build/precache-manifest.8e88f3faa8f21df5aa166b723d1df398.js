04d7c8e13b8ecafd4e97a0c2f1e177a386ff1590
Bryan Hunter <bdhunter3141@gmail.com>
2019-12-21 15:15:24 -0500

    Adding `build` to source control

---
+++ client/build/precache-manifest.8e88f3faa8f21df5aa166b723d1df398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761d15e0f52580375c344fe0b873c1c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31bb687dc5c2e93fd572",
+    "url": "/static/css/main.b24d7955.chunk.css"
+  },
+  {
+    "revision": "732b02447e92cbaed67c",
+    "url": "/static/js/2.ebdb8b5d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ebdb8b5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31bb687dc5c2e93fd572",
+    "url": "/static/js/main.3dbccefd.chunk.js"
+  },
+  {
+    "revision": "d0c744a0381f5f2761ac",
+    "url": "/static/js/runtime-main.9ec0b369.js"
+  }
+]);
